6bb6785134f455e973e166724dde813a581f3b0d
Matheus Alexandre <matheuscurses@gmail.com>
2024-08-13 19:31:15 -0300

    Validando despesas usando o if

---
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Create([FromBody] RequestRegisterExpenseJson request)
+    {
+        var useCase = new RegisterExpenseUseCase().Execute(request);
+
+        return Created(string.Empty, useCase);
+    }
+}
---
+++ src/CashFlow.Communication/Enums/EPaymentType.cs
@@ -0,0 +1,8 @@
+﻿namespace CashFlow.Communication.Enums;
+public enum EPaymentType
+{
+    Cash = 0,
+    CreditCard = 1,
+    DebitCard = 2,
+    Pix = 3
+}
