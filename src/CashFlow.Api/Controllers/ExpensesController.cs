5f9e4d4e251770e903db091f2fb6a6d8e693f1d9
Matheus Alexandre <matheuscurses@gmail.com>
2024-08-30 16:56:44 -0300

    Terminado a lista de exceções e feito o arquivo Resource

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,7 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
-using CashFlow.Communication.Responses;
-using CashFlow.Exception.ExceptionBase;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
@@ -12,25 +10,8 @@ public class ExpensesController : ControllerBase
     [HttpPost]
     public IActionResult Create([FromBody] RequestRegisterExpenseJson request)
     {
-        try
-        {
-            var response = new RegisterExpenseUseCase().Execute(request);
-
-            return Created(string.Empty, response);
-        }
-
-        catch (ErrorOnValidationException ex)
-        {
-            var errorResponse = new ResponseErrorJson(ex.Errors);
-
-            return BadRequest(errorResponse);
-        }
-
-        catch
-        {
-            var errorResponse = new ResponseErrorJson("unknown error");
-
-            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
-        }
+        var useCase = new RegisterExpenseUseCase();
+        var response = useCase.Execute(request);
+        return Created(string.Empty, response);
     }
 }
